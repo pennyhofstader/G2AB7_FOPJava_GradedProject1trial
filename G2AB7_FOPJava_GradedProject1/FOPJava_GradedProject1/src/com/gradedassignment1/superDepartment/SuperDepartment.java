763bfbc0d02dc964411fdea76803d9da45f2c44e
Khushi <anupreet.bedi12@gmail.com>
2022-10-06 16:49:13 +0530

    graded group assignment

---
+++ G2AB7_FOPJava_GradedProject1/FOPJava_GradedProject1/src/com/gradedassignment1/superDepartment/SuperDepartment.java
@@ -0,0 +1,20 @@
+package com.gradedassignment1.superDepartment;
+
+public class SuperDepartment {
+	// defining the methods
+	protected String departmentName() {
+		return "Super Department";
+	}
+
+	protected String getTodaysWork() {
+		return "No Work as of now";
+	}
+
+	protected String getWorkDeadline() {
+		return "Nil";
+	}
+
+	public String isTodayAHoliday() {
+		return "Today Is Not A Holiday";
+	}
+}
